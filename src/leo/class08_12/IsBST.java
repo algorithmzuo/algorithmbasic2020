f01fe6cc76d07ebb7af0acba81da06510e0bb095
Leo <582717189@qq.com>
2020-12-14 00:05:04 +0800

    练习

---
+++ src/leo/class08_12/IsBST.java
@@ -0,0 +1,241 @@
+package leo.class08_12;
+
+import java.util.ArrayList;
+
+/**
+ * @author Leo
+ * @ClassName IsBST
+ * @DATE 2020/12/11 6:05 下午
+ * @Description 判读是否是搜索二叉树
+ * 每棵树的左部分比自己小, 右部分比自己大
+ * 经典搜索二叉树没有重复值
+ * 1.Left subtree is Binary Search Tree
+ * 2.right subtree is Binary Search Tree
+ * 3.max value of left subtree less than the value of head
+ * 4.min value of right subtree greater than the value of head
+ */
+public class IsBST {
+
+    static class Code {
+        public static boolean isBST(Node head){
+            if (head == null) {
+                return true;
+            }
+            return process(head).is;
+        }
+
+
+        public static Info process(Node node) {
+            if (node == null) {
+                return null;
+            }
+            Info left = process(node.left);
+            Info right = process(node.right);
+
+            boolean is = true;
+            int max = node.value;
+            int min = node.value;
+            if (left != null) {
+                max = Math.max(max, left.max);
+                min = Math.min(min, left.min);
+            }
+            if (right != null) {
+                min = Math.min(min, right.min);
+                max = Math.max(max, right.max);
+            }
+            if (left != null && !left.is) {
+                is = false;
+            }
+            if (right != null && !right.is) {
+                is = false;
+            }
+            if (left != null && left.max >= node.value) {
+                is = false;
+            }
+            if (right != null && right.min <= node.value) {
+                is = false;
+            }
+            return new Info(is, max, min);
+        }
+
+        static class Info{
+            boolean is;
+            int max;
+            int min;
+            public Info(boolean is,int max,int min){
+                this.is = is;
+                this.max = max;
+                this.min = min;
+            }
+        }
+
+
+    }
+
+    static  class Code1{
+        public static boolean isBST(Node head){
+            if (head == null) {
+                return true;
+            }
+
+            return process(head).isBST;
+        }
+
+        public static Info process(Node node){
+            if (node == null) {
+                return null;
+            }
+
+            Info left = process(node.left);
+            Info right = process(node.right);
+            boolean isBST = true;
+            int max = node.value;
+            int min = node.value;
+            if (left != null) {
+                max = Math.max(max, left.max);
+                min = Math.min(min, left.min);
+
+            }
+            if (right != null) {
+                max = Math.max(max, right.max);
+                min = Math.min(min, right.min);
+            }
+            if (left != null && !left.isBST) {
+                isBST = false;
+            }
+            if (right != null && !right.isBST) {
+                isBST = false;
+            }
+            if (left != null && left.max >= node.value) {
+                isBST = false;
+            }
+            if (right != null && right.min <= node.value) {
+                isBST = false;
+            }
+            return new Info(isBST, max, min);
+        }
+
+
+
+        static class Info{
+            boolean isBST;
+            int max;
+            int min;
+            public Info(boolean is,int ma,int mi){
+                this.isBST = is;
+                this.max = ma;
+                this.min = mi;
+            }
+        }
+
+    }
+
+    static class Code2{
+        public static boolean isBST(Node head) {
+            if (head == null) {
+                return true;
+            }
+            return process(head).isBST;
+        }
+        public static Info process(Node node){
+            if (node == null) {
+                return null;
+            }
+            Info left = process(node.left);
+            Info right = process(node.right);
+            boolean isBST = false;
+            int max = node.value;
+            int min = node.value;
+            if (left != null) {
+                max = Math.max(max, left.max);
+                min = Math.min(min, left.min);
+            }
+            if (right != null) {
+                max = Math.max(max, right.max);
+                min = Math.min(min, right.min);
+            }
+            if (
+                    (left == null ? true : left.isBST)
+                    && (right == null ? true : right.isBST)
+                    && (left == null ? true : left.max < node.value)
+                    && (right == null ? true : right.min > node.value)
+            ) {
+                isBST = true;
+            }
+            return new Info(isBST, max, min);
+        }
+
+
+        static class Info{
+            boolean isBST;
+            int max;
+            int min;
+            public Info(boolean is,int ma,int mi){
+                this.isBST = is;
+                this.max = ma;
+                this.min = mi;
+            }
+
+        }
+    }
+
+
+    static class Logarithm{
+        public static boolean isBST(Node head) {
+            if (head == null) {
+                return true;
+            }
+            ArrayList<Node> list = new ArrayList<>();
+            in(head, list);
+            for (int i = 1; i < list.size(); i++) {
+                if (list.get(i).value <= list.get(i - 1).value) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static void in(Node node, ArrayList<Node> list) {
+            if (node == null) {
+                return;
+            }
+            in(node.left, list);
+            list.add(node);
+            in(node.right, list);
+        }
+
+    }
+
+
+    public static void main(String[] args){
+        int level = 4;
+        int range = 100;
+        int test = 100000;
+        System.out.println("start!");
+        for (int i = 0; i < test; i++) {
+            Node head = generateRandomNode(level, range);
+            boolean codec = Code2.isBST(head);
+            boolean logarithm = Logarithm.isBST(head);
+            if (codec != logarithm) {
+                System.out.println("fuck!");
+                break;
+            }
+        }
+        System.out.println("end!");
+    }
+
+    public static Node generateRandomNode(int level, int range) {
+        return generate(1, level, range);
+    }
+
+    private static Node generate(int i, int n, int range) {
+        if (i > n || Math.random() < 0.5) {
+            return null;
+        }
+        Node head = new Node((int) (range * Math.random()));
+        head.left = generate(i + 1, n, range);
+        head.right = generate(i + 1, n, range);
+        return head;
+    }
+}
---
+++ src/leo/class08_12/IsCBT.java
@@ -0,0 +1,169 @@
+package leo.class08_12;
+
+import javax.swing.plaf.TreeUI;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author Leo
+ * @ClassName IsCBT
+ * @DATE 2020/12/11 3:22 下午
+ * @Description
+ * 判断二叉树是否是完全二叉树
+ * 1.如果一个树只有右节点没有左节点 直接false
+ * 2.当第一次遇到左右不双全的树时,剩下的节点必须是叶节点
+ */
+public class IsCBT {
+
+    public static void main(String[] args){
+        int level = 4;
+        int range = 100;
+        int testTime = 10000;
+        System.out.println("start");
+
+        for (int i = 0; i < testTime; i++) {
+            Node head = generateRandomNode(level, range);
+            boolean queue = ByQueue.isCBT1(head);
+            boolean recursion = Recursion.isCBT(head);
+            if (queue != recursion) {
+                System.out.println("fuck!!!");
+                break;
+            }
+
+        }
+        System.out.println("end");
+
+    }
+    static Node generateRandomNode(int n,int r){
+        return generateNode(1,n,r);
+    }
+    static Node generateNode(int i,int n,int r){
+        if (i > n || Math.random() < 0.5) {
+            return null;
+        }
+        Node head = new Node((int) (r * Math.random()));
+        head.left = generateNode(i + 1, n, r);
+        head.right = generateNode(i + 1, n, r);
+        return head;
+    }
+
+}
+
+class ByQueue{
+    public static boolean isCBT(Node head) {
+        if (head == null) {
+            return true;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(head);
+        boolean leaf = false;
+        Node cur = null;
+        Node l = null;
+        Node r = null;
+        while (!queue.isEmpty()) {
+            cur = queue.poll();
+            l = cur.left;
+            r = cur.right;
+            if ( //是叶节点并且有子孩子
+                (leaf && (l != null || r != null))
+                ||
+                (l == null && r != null)//当没有左节点时
+            ) {
+                return false;
+            }
+            if (l != null) {
+                queue.offer(l);
+            }
+            if (r != null) {
+                queue.offer(r);
+            }
+            if (l == null || r == null) {
+                leaf = true;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isCBT1(Node head){
+        if (head == null) {
+            return true;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(head);
+        Node cur;
+        Node l = null;
+        Node r = null;
+        boolean leaf = false;
+        while (!queue.isEmpty()) {
+            cur = queue.poll();
+            l = cur.left;
+            r = cur.right;
+            if (l!= null) {
+                queue.offer(l);
+            }
+            if (r != null) {
+                queue.offer(r);
+            }
+            if ((leaf &&
+                    (l != null || r != null))
+                    || (l == null && r != null)
+            ) {
+
+                return false;
+            }
+            if (l == null || r == null) {
+                leaf = true;
+            }
+        }
+        return true;
+    }
+}
+
+class Recursion{
+    public static boolean isCBT(Node head) {
+        return process(head).isCBT;
+
+    }
+    public static Info process(Node node){
+        if (node == null) {
+            return new Info(true, true, 0);
+        }
+        Info left = process(node.left);
+        Info right = process(node.right);
+        int height = Math.max(left.height, right.height) + 1;
+        boolean isFull = left.isFull && right.isFull && right.height == left.height;
+        boolean isCBT = false;
+        if (isFull) {
+            isCBT = true;
+        }else{
+            //如果左右都不是满二叉树的情况下
+            //左树是满二叉树并且右树是满二叉树
+            if (left.isCBT && right.isCBT) {
+                //左树是二叉树,右树是满二叉树,左树的高度比右树高度大1
+                if (left.isCBT && right.isFull && left.height == right.height + 1) {
+                    isCBT = true;
+                }
+                //左树是满二叉树,右树是满二叉树,左树的高度比右树高度大1
+                if (left.isFull && right.isFull && left.height == right.height + 1) {
+                    isCBT = true;
+                }
+                //左树是满二叉树,右树是二叉树,左树高度与右树高度一样
+                if (left.isFull && right.isCBT && right.height == left.height) {
+                    isCBT = true;
+                }
+
+            }
+        }
+        return new Info(isFull, isCBT, height);
+    }
+    static class Info{
+        boolean isFull;
+        boolean isCBT;
+        int height;
+        public Info(boolean f,boolean c,int h){
+            this.isFull = f;
+            this.isCBT = c;
+            this.height = h;
+        }
+    }
+}
