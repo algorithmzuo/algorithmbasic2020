5cf6403b9c7e28a8bfe725e663d643093b9acf58
左程云 <algorithmzuo@163.com>
2020-12-22 16:40:06 +0800

    add problems about UnionFind

---
+++ src/class09_15/Code01_FriendCircles.java
@@ -0,0 +1,69 @@
+package class09_15;
+
+// 本题为leetcode原题
+// 测试链接：https://leetcode.com/problems/friend-circles/
+// 可以直接通过
+public class Code01_FriendCircles {
+
+	public static class UnionFind {
+		private int[] parent;
+		private int[] size;
+		private int[] help;
+		private int sets;
+
+		public UnionFind(int N) {
+			parent = new int[N];
+			size = new int[N];
+			help = new int[N];
+			sets = N;
+			for (int i = 0; i < N; i++) {
+				parent[i] = i;
+				size[i] = 1;
+			}
+		}
+
+		private int find(int i) {
+			int hi = 0;
+			while (i != parent[i]) {
+				help[hi++] = i;
+				i = parent[i];
+			}
+			for (hi--; hi >= 0; hi--) {
+				parent[help[hi]] = i;
+			}
+			return i;
+		}
+
+		public void union(int i, int j) {
+			int f1 = find(i);
+			int f2 = find(j);
+			if (f1 != f2) {
+				if (size[f1] >= size[f2]) {
+					size[f1] += size[f2];
+					parent[f2] = f1;
+				} else {
+					size[f2] += size[f1];
+					parent[f1] = f2;
+				}
+				sets--;
+			}
+		}
+
+		public int sets() {
+			return sets;
+		}
+	}
+
+	public static int findCircleNum(int[][] M) {
+		int N = M.length;
+		UnionFind unionFind = new UnionFind(N);
+		for (int i = 0; i < N; i++) {
+			for (int j = i + 1; j < N; j++) {
+				if (M[i][j] == 1) {
+					unionFind.union(i, j);
+				}
+			}
+		}
+		return unionFind.sets();
+	}
+}
---
+++ src/class09_15/Code03_NumberOfIslandsII.java
@@ -0,0 +1,94 @@
+package class09_15;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Code03_NumberOfIslandsII {
+
+	public static List<Integer> numIslands2(int m, int n, int[][] positions) {
+		UnionFind unionFind = new UnionFind(m, n);
+		List<Integer> ans = new ArrayList<>();
+		for (int[] position : positions) {
+			unionFind.connect(position[0], position[1]);
+			ans.add(unionFind.sets());
+		}
+		return ans;
+	}
+
+	public static class UnionFind {
+		private int[] parent;
+		private int[] size;
+		private int[] help;
+		private final int col;
+		private final int row;
+		private int sets;
+
+		public UnionFind(int m, int n) {
+			row = m;
+			col = n;
+			sets = 0;
+			int len = row * col;
+			parent = new int[len];
+			size = new int[len];
+			help = new int[len];
+		}
+
+		private int index(int r, int c) {
+			return r * col + c;
+		}
+
+		private int find(int i) {
+			int hi = 0;
+			while (i != parent[i]) {
+				help[hi++] = i;
+				i = parent[i];
+			}
+			for (hi--; hi >= 0; hi--) {
+				parent[help[hi]] = i;
+			}
+			return i;
+		}
+
+		private void union(int r1, int c1, int r2, int c2) {
+			if (r1 < 0 || r1 == row || r2 < 0 || r2 == row || c1 < 0 || c1 == col || c2 < 0 || c2 == col) {
+				return;
+			}
+			int i1 = index(r1, c1);
+			int i2 = index(r2, c2);
+			if (size[i1] == 0 || size[i2] == 0) {
+				return;
+			}
+			int f1 = find(i1);
+			int f2 = find(i2);
+			if (f1 != f2) {
+				if (size[f1] >= size[f2]) {
+					size[f1] += size[f2];
+					parent[f2] = f1;
+				} else {
+					size[f2] += size[f1];
+					parent[f1] = f2;
+				}
+				sets--;
+			}
+		}
+
+		public void connect(int i, int j) {
+			int index = index(i, j);
+			if (size[index] == 0) {
+				parent[index] = index;
+				size[index] = 1;
+				sets++;
+				union(i - 1, j, i, j);
+				union(i + 1, j, i, j);
+				union(i, j - 1, i, j);
+				union(i, j + 1, i, j);
+			}
+		}
+
+		public int sets() {
+			return sets;
+		}
+
+	}
+
+}
