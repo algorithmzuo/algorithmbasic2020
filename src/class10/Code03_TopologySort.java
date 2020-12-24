8e8b7abbf3b07756b46ee01c82db3201d1a0ec3e
左程云 <algorithmzuo@163.com>
2020-12-24 11:46:40 +0800

    modify code

--- src/class10/Code03_TopologySort.java
+++ src/class10/Code03_TopologySort.java
@@ -10,20 +10,14 @@ public class Code03_TopologySort {
 
 	// directed graph and no loop
 	public static List<Node> sortedTopology(Graph graph) {
-		// key：某一个node
-		// value：剩余的入度
 		HashMap<Node, Integer> inMap = new HashMap<>();
-		// 剩余入度为0的点，才能进这个队列
 		Queue<Node> zeroInQueue = new LinkedList<>();
-		
-		
 		for (Node node : graph.nodes.values()) {
 			inMap.put(node, node.in);
 			if (node.in == 0) {
 				zeroInQueue.add(node);
 			}
 		}
-		// 拓扑排序的结果，依次加入result
 		List<Node> result = new ArrayList<>();
 		while (!zeroInQueue.isEmpty()) {
 			Node cur = zeroInQueue.poll();
