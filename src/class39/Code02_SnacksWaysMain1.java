e5bfd19b9f00f17256dcd3a173b13bbcd56073de
algorithmzuo <algorithmzuo@163.com>
2022-06-18 11:03:09 +0800

    modify code

--- src/class39/Code02_SnacksWaysMain1.java
+++ src/class39/Code02_SnacksWaysMain1.java
@@ -1,17 +1,18 @@
-//不要拷贝包信息的内容
+// 不要拷贝包信息的内容
 package class39;
 
-//本文件是Code02_SnacksWays问题的牛客题目解答
-//但是用的分治的方法
-//这是牛客的测试链接：
-//https://www.nowcoder.com/questionTerminal/d94bb2fa461d42bcb4c0f2b94f5d4281
-//把如下的全部代码拷贝进编辑器（java）
-//可以直接通过
+// 课堂版本
+// 本文件是Code02_SnacksWays问题的牛客题目解答
+// 但是用的分治的方法
+// 这是牛客的测试链接：
+// https://www.nowcoder.com/questionTerminal/d94bb2fa461d42bcb4c0f2b94f5d4281
+// 把如下的全部代码拷贝进编辑器（java）
+// 可以直接通过
 import java.util.Map.Entry;
 import java.util.Scanner;
 import java.util.TreeMap;
 
-public class Code02_SnacksWaysMain {
+public class Code02_SnacksWaysMain1 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
