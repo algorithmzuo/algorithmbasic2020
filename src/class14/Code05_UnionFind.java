39c0be907cd06f982f1e189297d34907a80ef624
algorithmzuo <algorithmzuo@163.com>
2023-03-31 23:04:42 +0800

    modify code

--- src/class14/Code05_UnionFind.java
+++ src/class14/Code05_UnionFind.java
@@ -14,64 +14,59 @@ import java.util.Stack;
 // 一定要看！
 public class Code05_UnionFind {
 
-	public static class Node<V> {
-		V value;
-
-		public Node(V v) {
-			value = v;
-		}
-	}
-
+	// 课上讲的时候
+	// 包了一层
+	// 其实不用包一层哦
 	public static class UnionFind<V> {
-		public HashMap<V, Node<V>> nodes;
-		public HashMap<Node<V>, Node<V>> parents;
-		public HashMap<Node<V>, Integer> sizeMap;
+		public HashMap<V, V> father;
+		public HashMap<V, Integer> size;
 
 		public UnionFind(List<V> values) {
-			nodes = new HashMap<>();
-			parents = new HashMap<>();
-			sizeMap = new HashMap<>();
+			father = new HashMap<>();
+			size = new HashMap<>();
 			for (V cur : values) {
-				Node<V> node = new Node<>(cur);
-				nodes.put(cur, node);
-				parents.put(node, node);
-				sizeMap.put(node, 1);
+				father.put(cur, cur);
+				size.put(cur, 1);
 			}
 		}
 
 		// 给你一个节点，请你往上到不能再往上，把代表返回
-		public Node<V> findFather(Node<V> cur) {
-			Stack<Node<V>> path = new Stack<>();
-			while (cur != parents.get(cur)) {
+		public V findFather(V cur) {
+			Stack<V> path = new Stack<>();
+			while (cur != father.get(cur)) {
 				path.push(cur);
-				cur = parents.get(cur);
+				cur = father.get(cur);
 			}
 			while (!path.isEmpty()) {
-				parents.put(path.pop(), cur);
+				father.put(path.pop(), cur);
 			}
 			return cur;
 		}
 
 		public boolean isSameSet(V a, V b) {
-			return findFather(nodes.get(a)) == findFather(nodes.get(b));
+			return findFather(a) == findFather(b);
 		}
 
 		public void union(V a, V b) {
-			Node<V> aHead = findFather(nodes.get(a));
-			Node<V> bHead = findFather(nodes.get(b));
-			if (aHead != bHead) {
-				int aSetSize = sizeMap.get(aHead);
-				int bSetSize = sizeMap.get(bHead);
-				Node<V> big = aSetSize >= bSetSize ? aHead : bHead;
-				Node<V> small = big == aHead ? bHead : aHead;
-				parents.put(small, big);
-				sizeMap.put(big, aSetSize + bSetSize);
-				sizeMap.remove(small);
+			V aFather = findFather(a);
+			V bFather = findFather(b);
+			if (aFather != bFather) {
+				int aSize = size.get(aFather);
+				int bSize = size.get(bFather);
+				if (aSize >= bSize) {
+					father.put(bFather, aFather);
+					size.put(aFather, aSize + bSize);
+					size.remove(bFather);
+				} else {
+					father.put(aFather, bFather);
+					size.put(bFather, aSize + bSize);
+					size.remove(aFather);
+				}
 			}
 		}
 
 		public int sets() {
-			return sizeMap.size();
+			return size.size();
 		}
 
 	}
